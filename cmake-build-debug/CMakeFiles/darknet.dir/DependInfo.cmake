2e54607d25e4d4d495a8c64c4bcac03cc2d7faf4
Don Tolley <don@brightlineinteractive.com>
2017-01-14 13:10:09 -0500

    working on cmake

---
+++ cmake-build-debug/CMakeFiles/darknet.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/activation_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/activation_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/activations.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/activations.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/art.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/art.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/avgpool_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/avgpool_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/batchnorm_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/batchnorm_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/blas.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/blas.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/box.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/box.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/captcha.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/captcha.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/cifar.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/cifar.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/classifier.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/classifier.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/coco.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/coco.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/col2im.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/col2im.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/compare.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/compare.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/connected_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/connected_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/convolutional_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/convolutional_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/cost_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/cost_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/crnn_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/crnn_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/crop_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/crop_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/cuda.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/cuda.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/darknet.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/darknet.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/data.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/data.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/deconvolutional_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/deconvolutional_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/demo.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/demo.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/detection_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/detection_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/detector.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/detector.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/dice.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/dice.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/dropout_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/dropout_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/gemm.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/gemm.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/go.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/go.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/gru_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/gru_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/im2col.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/im2col.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/image.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/image.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/list.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/list.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/local_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/local_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/matrix.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/matrix.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/maxpool_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/maxpool_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/network.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/network.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/nightmare.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/nightmare.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/normalization_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/normalization_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/option_list.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/option_list.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/parser.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/parser.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/region_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/region_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/reorg_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/reorg_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/rnn.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/rnn.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/rnn_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/rnn_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/rnn_vid.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/rnn_vid.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/route_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/route_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/shortcut_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/shortcut_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/softmax_layer.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/softmax_layer.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/super.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/super.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/swag.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/swag.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/tag.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/tag.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/tree.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/tree.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/utils.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/utils.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/voxel.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/voxel.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/writing.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/writing.c.o"
+  "/Users/Don/Documents/Projects/darknetmain/darknet/src/yolo.c" "/Users/Don/Documents/Projects/darknetmain/darknet/cmake-build-debug/CMakeFiles/darknet.dir/src/yolo.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/darknet.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/Users/Don/Documents/Projects/darknetmain/lib/libdarknet.a"
+)
